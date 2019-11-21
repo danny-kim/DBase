b8142b5db38500bbf9220e9f3547fdda32248208
dannykim <danny.kuy@pathmazing.com>
2019-11-21 11:48:52 +0700

    update

--- dbase/src/main/java/com/example/dbase/Utils.kt
+++ dbase/src/main/java/com/example/dbase/Utils.kt
@@ -3,6 +3,7 @@ package com.example.dbase
 import android.content.Context
 import android.content.Context.TELEPHONY_SERVICE
 import android.telephony.TelephonyManager
+import java.util.*
 
 
 fun getCountryCode(context: Context): String {
@@ -11,7 +12,7 @@ fun getCountryCode(context: Context): String {
     val arrayCodes = context.resources.getStringArray(R.array.CountryCodes)
     for (i in arrayCodes.indices) {
         val arrayCode = arrayCodes[i].split(",")
-        if (arrayCode[1].trim () == manager.simCountryIso.trim()) {
+        if (arrayCode[1].trim () == manager.simCountryIso.trim().toUpperCase(Locale.getDefault())) {
             code = arrayCode[0]
             break
         }
